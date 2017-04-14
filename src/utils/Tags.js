1370d1890b78ddfb0b56d2e77d202c82edc3be74
JenniferWongL4gg <jennifer@lawyersforgoodgovernment.org>
2017-04-14 18:40:00 -0500

    Add Denial, Spicer, Associates and Pre-Election Tags

--- src/utils/Tags.js
+++ src/utils/Tags.js
@@ -2,5 +2,21 @@ export default {
 	EXAMPLE: {
 		title: 'Example Tag',
 		description: 'This is an example tag'
+	},
+	DENIAL: {
+		title: 'Denial',
+		description: 'Denial of contact with Russia'
+	},
+	SEAN_SPICER: {
+		title: 'Sean Spicer',
+		description: 'Events involving Sean Spicer'
+	},
+	CONTACT_BEFORE_ELECTION: {
+		title: 'Pre-Election',
+		description: 'Contact with Russia Prior to the 2016 Election'
+	},
+	TRUMP_ASSOCIATES: {
+		title: 'Trump Associates',
+		description: 'Contact with Russia by Trump Associates'
 	}
 }
