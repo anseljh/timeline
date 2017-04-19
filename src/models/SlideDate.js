c30061446f4afec8516b4e93161036e2aaa8cd9e
Jacob LeGrone <git@jacoblegrone.com>
2017-04-19 10:52:37 -0400

    Parse hour and minute from ISO 8601 strings

--- src/models/SlideDate.js
+++ src/models/SlideDate.js
@@ -1,11 +1,16 @@
 import moment from 'moment'
 
 export default class SlideDate {
-	constructor(dateISOString, displayDateString = null) {
-		const dateArray = dateISOString.split('-')
-		const date = moment(dateISOString, 'YYYY-MM-DD')
+	constructor(ISO8601String, displayDateString = null) {
+		const [dateString, timeString] = ISO8601String.split('T')
+		const dateArray = dateString.split('-')
+		const date = moment(ISO8601String)
 
 		this.year = date.year()
+		if (timeString) {
+			this.hour = date.hour()
+			this.minute = date.minute()
+		}
 		if (dateArray.length >= 3) {
 			this.day = date.date()
 		}
