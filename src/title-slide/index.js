84158ab650a51b5567d17e12cad80c585b5e5ae9
Ansel Halliburton <anseljh@Humanist.local>
2017-05-05 17:55:29 -0700

    Use SlideMedia to load image nicely

--- src/title-slide/index.js
+++ src/title-slide/index.js
@@ -1,5 +1,6 @@
 import Slide from 'models/Slide'
 import SlideContent from 'models/SlideContent'
+import SlideMedia from 'models/SlideMedia'
 
 import title from './slide-title.html'
 import text from './slide-text.html'
@@ -7,7 +8,7 @@ import text from './slide-text.html'
 const titleSlide = new Slide(
 	null,
 	new SlideContent(title, text),
-	null,
+	new SlideMedia('/images/l4gg-logo-500.png'),
 	null
 )
 
